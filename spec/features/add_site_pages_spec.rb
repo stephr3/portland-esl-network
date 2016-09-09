8f2b9530e568f26513580cedc18eb89fef667433
Stephanie Gurung <stephr3@gmail.com>
2016-09-09 14:08:19 -0700

    pass integration specs for adding a new class

---
+++ spec/features/add_site_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "the add a site process" do
+  it "adds a new site" do
+    visit sites_path
+    click_link 'Add a New Class'
+    fill_in 'Name', with: 'ESL Class'
+    fill_in 'Address', with: "123 ABC Street"
+    fill_in 'City', with: "Portland"
+    select('OR', from: 'site_state')
+    select('Downtown', from: 'site_region')
+    click_on 'Create Site'
+    expect(page).to have_content 'ESL Classes'
+  end
+
+  it "gives error when no name is entered" do
+    visit new_site_path
+    click_on 'Create Site'
+    expect(page).to have_content 'errors'
+  end
+end
