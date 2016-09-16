3eaf1c82451daf80056159b105e2a4c8045daeaf
Stephanie Gurung <stephr3@gmail.com>
2016-09-16 12:37:03 -0700

    filter new, edit, and destroy sites to only be accessible with admin login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < ActionController::Base
 	  @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
 	end
 
+  def is_admin
+    if !current_admin
+      flash[:alert] = 'You must log in as an admin to do this task'
+      redirect_to log_in_path
+    end
+  end
+
   helper_method :current_admin
 end
