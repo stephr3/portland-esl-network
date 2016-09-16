dd31b1f94371d0bb7406ad8d5eee553c3e5ff2ec
Stephanie Gurung <stephr3@gmail.com>
2016-09-16 15:21:35 -0700

    allow admin to edit their own post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,9 +20,20 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @admin = Admin.find(params[:admin_id])
+    @post = Post.find(params[:id])
   end
 
   def update
+    @admin = Admin.find(params[:admin_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "You have successfully submitted your edits for this post."
+      redirect_to posts_path
+    else
+      flash[:alert] = "We're sorry, your post updates have not been successfully processed."
+      render :edit
+    end
   end
 
   def destroy
