92e86c52fbfd903091b4676b804422e872ec5c69
Stephanie Gurung <stephr3@gmail.com>
2016-09-16 09:39:05 -0700

    reach 100 percent simplecov test coverage

--- spec/features/view_sites_pages_spec.rb
+++ spec/features/view_sites_pages_spec.rb
@@ -7,10 +7,52 @@ describe 'the view sites process' do
     expect(page).to have_content 'Sunshine ESL Class'
   end
 
-  it 'filters sites by region' do
+  it 'filters sites by North/Northeast region' do
+    FactoryGirl.create(:site)
+    visit root_path
+    click_on 'North/Northeast'
+    expect(page).to have_no_content 'Sunshine ESL Class'
+  end
+  it 'filters sites by South/Southeast region' do
+    FactoryGirl.create(:site)
+    visit root_path
+    click_on 'South/Southeast'
+    expect(page).to have_no_content 'Sunshine ESL Class'
+  end
+  it 'filters sites by Southwest region' do
+    FactoryGirl.create(:site)
+    visit root_path
+    click_on 'Southwest'
+    expect(page).to have_no_content 'Sunshine ESL Class'
+  end
+  it 'filters sites by Downtown region' do
+    FactoryGirl.create(:site, region: 'Gresham')
+    visit root_path
+    click_on 'Downtown'
+    expect(page).to have_no_content 'Sunshine ESL Class'
+  end
+  it 'filters sites by Gresham region' do
     FactoryGirl.create(:site)
     visit root_path
     click_on 'Gresham'
+    expect(page).to have_no_content 'Sunshine ESL Class'
+  end
+  it 'filters sites by Washington County region' do
+    FactoryGirl.create(:site)
+    visit root_path
+    click_on 'Washington County'
+    expect(page).to have_no_content 'Sunshine ESL Class'
+  end
+  it 'filters sites by Clark County region' do
+    FactoryGirl.create(:site)
+    visit root_path
+    click_on 'Clark County'
+    expect(page).to have_no_content 'Sunshine ESL Class'
+  end
+  it 'filters sites by Other Areas region' do
+    FactoryGirl.create(:site)
+    visit root_path
+    click_on 'Other Areas'
     expect(page).to have_no_content 'Sunshine ESL Class'
   end
 end
