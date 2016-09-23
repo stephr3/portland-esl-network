6c929ac94c89cb5f91a850307462406ea337d502
Stephanie Gurung <stephr3@gmail.com>
2016-09-23 09:16:21 -0700

    add and pass post model specs, clean up ruby syntax

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
   validates :title, :content, presence: true
+  validates :title, length: { maximum: 200 }
+  validates :content, length: { maximum: 1000 }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 describe Post do
+  it { should belong_to :admin }
+
   it { should validate_presence_of :title }
   it { should validate_presence_of :content }
-  it { should belong_to :admin }
+  it { should validate_length_of(:title).is_at_most(200) }
+  it { should validate_length_of(:content).is_at_most(1000) }
 end
