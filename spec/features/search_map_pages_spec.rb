955d8b28e8324ce86f01a71145777710219ee894
Stephanie Gurung <stephr3@gmail.com>
2016-09-30 11:09:51 -0700

    pass integration spec for map search error handling

--- spec/features/search_map_pages_spec.rb
+++ spec/features/search_map_pages_spec.rb
@@ -7,4 +7,10 @@ describe "the search map process", vcr: true do
     click_button 'Search'
     expect(page).to have_content 'Your search: 97211'
   end
+  it "displays an error if an invalid zip code has been entered" do
+    visit root_path
+    fill_in 'center-map', with: 'invalid'
+    click_button 'Search'
+    expect(page).to have_content 'Please enter a valid zip code.'
+  end
 end
