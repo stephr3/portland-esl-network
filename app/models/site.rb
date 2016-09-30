eeaff868238fe06052d5e6ad9d0b40eb3a83b403
Stephanie Gurung <stephr3@gmail.com>
2016-09-30 08:56:38 -0700

    display error when address of a submitted or edited class cannot be geocoded

--- app/models/site.rb
+++ app/models/site.rb
@@ -44,9 +44,20 @@ class Site < ActiveRecord::Base
 
   #For Geocoder Functionality
   geocoded_by :geocoder_address
-  after_validation :geocode, if: ->(obj){ obj.geocoder_address.present? }
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? or obj.city_changed? or obj.state_changed? or obj.zip_changed? }
+  after_validation :lat_changed?
 
   def geocoder_address
     [address, city, state, zip].compact.join(', ')
+  end
+
+  def lat_changed?
+    if (self.address_changed?) or (self.city_changed?) or (self.state_changed?) or (self.zip_changed?)
+        if !(self.latitude_changed?)
+            self.errors.add(:address, "is not valid")
+            return false
+        end
+    end
+    return true
   end
 end
