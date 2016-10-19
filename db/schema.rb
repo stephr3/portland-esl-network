496f3bf44193137949092b4da39e40ab8ff42929
Stephanie Gurung <stephr3@gmail.com>
2016-10-19 10:50:32 -0700

    migrate search tool

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920025112) do
+ActiveRecord::Schema.define(version: 20160930151436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20160920025112) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "state"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
