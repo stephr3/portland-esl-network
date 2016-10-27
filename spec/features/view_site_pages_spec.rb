c27ef623a637f1bf1ffe867c12af0871df24e88e
Stephanie Gurung <stephr3@gmail.com>
2016-10-26 18:48:50 -0700

    display full class information to admins

--- spec/features/view_site_pages_spec.rb
+++ spec/features/view_site_pages_spec.rb
@@ -7,4 +7,16 @@ describe 'the view a site process', vcr: true do
     click_on 'Sunshine ESL Class'
     expect(page).to have_content '123 ABC St'
   end
+
+  it 'only shows admin information if admin is logged in' do
+    FactoryGirl.create(:site)
+    FactoryGirl.create(:admin)
+    visit admin_path
+    fill_in 'Username', with: 'admin'
+    fill_in 'Password', with: 'admin'
+    click_button 'Log in'
+    visit sites_path
+    click_on 'Sunshine ESL Class'
+    expect(page).to have_content 'Last contacted 9/16 by Stephanie'
+  end
 end
