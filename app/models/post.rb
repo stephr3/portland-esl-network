bbca94f599c584e0314d5b8af602f94f656b0b57
Stephanie Gurung <stephr3@gmail.com>
2016-10-27 19:41:13 -0700

    display buttons for mobile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
-  paginates_per 2
+  paginates_per 3
   validates :title, :content, presence: true
   validates :title, length: { maximum: 200 }
   validates :content, length: { maximum: 1000 }
