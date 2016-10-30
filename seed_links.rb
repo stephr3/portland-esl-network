e83c3d904ee40a2bbf221c39cd6033d6779ad882
Stephanie Roth <stephr3@gmail.com>
2016-10-30 13:09:21 -0700

    seed more links and sites; fix minor user interface issues

---
+++ seed_links.rb
@@ -0,0 +1,65 @@
+Link.create(
+  title: "Multnomah County Library Web Resources",
+  url: "https://multcolib.org/learn-english",
+  description: "Multnomah Public Library's links to multiple websites",
+  tag: "Multi-Level",
+  page: "Student Learning Resources"
+)
+Link.create(
+  title: "BBC Dance Mat Typing",
+  url: "http://www.bbc.co.uk/guides/z3c6tfr#zgkpn39",
+  description: "Fun, free, interactive typing tutor program",
+  tag: "Learn to type",
+  page: "Student Learning Resources"
+)
+Link.create(
+  title: "Pumarosa",
+  url: "http://www.pumarosa.com",
+  description: "Fast, fun and free to learn English. It's specially designed to help Spanish-speaking students with difficulties learning, but anyone can use it! ",
+  tag: "Beginning",
+  page: "Student Learning Resources"
+)
+
+Link.create(
+  title: "Immigrant and Refugee Portland Resource Directory",
+  url: "http://lcsnw.org/portland/pathways/ResourceDirectory.pdf",
+  description: "",
+  tag: "Immigrants and Refugees",
+  page: "Teaching Resources"
+)
+Link.create(
+  title: "Portland Literacy",
+  url: "http://www.portlandliteracy.org/",
+  description: "Free resource for beginning Literacy lessons. There are four levels of packets for tutors and for students.",
+  tag: "Literacy",
+  page: "Teaching Resources"
+)
+Link.create(
+  title: "CSpan Classroom Deliberations",
+  url: "http://spanclassroomdeliberations.org",
+  description: "Designed to engage students in classroom deliberations about current issues being debated in congress.  Lesson plans for teachers.",
+  tag: "Conversation",
+  page: "Teaching Resources"
+)
+
+Link.create(
+  title: "Refugees from Burma: Their Backgrounds & Experiences.pdf",
+  url: "http://portlandesl.pbworks.com/w/file/17833085/Refugees%20from%20Burma%3A%20Their%20Backgrounds%20%26%20Experiences.pdf",
+  description: "",
+  tag: "",
+  page: "Refugee Backgrounders"
+)
+Link.create(
+  title: "The Somali Bantu: Their History and Culture.pdf",
+  url: "http://portlandesl.pbworks.com/w/file/17833086/The%20Somali%20Bantu%3A%20Their%20History%20and%20Culture.pdf",
+  description: "",
+  tag: "",
+  page: "Refugee Backgrounders"
+)
+Link.create(
+  title: "Muslim Refugees in the US: A Guide for Service Providers.pdf",
+  url: "http://portlandesl.pbworks.com/w/file/17833080/Muslim%20Refugees%20in%20the%20US%3A%20A%20Guide%20for%20Service%20Providers.pdf",
+  description: "",
+  tag: "",
+  page: "Refugee Backgrounders"
+)
