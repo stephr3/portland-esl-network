127582b7cd85206d818d33be1baa1534bc9bc051
Stephanie Gurung <stephr3@gmail.com>
2017-03-26 00:59:44 -0700

    update email

--- app/mailers/user_submitted_updates_mailer.rb
+++ app/mailers/user_submitted_updates_mailer.rb
@@ -3,12 +3,12 @@ class UserSubmittedUpdatesMailer < ApplicationMailer
 
   def class_submitted(site)
     @site = site
-    mail(to: "stephr3@gmail.com", subject: "Portland ESL Network - #{@site.user_name} has Submitted a New Class")
+    mail(to: "heidi.dryden@gmail.com", subject: "Portland ESL Network - #{@site.user_name} has Submitted a New Class")
   end
 
   def class_updated(site, update)
     @site = site
     @update = update
-    mail(to: "stephr3@gmail.com", subject: "Portland ESL Network - #{@update.user_name} has Requested Updates for #{@site.name}")
+    mail(to: "heidi.dryden@gmail.com", subject: "Portland ESL Network - #{@update.user_name} has Requested Updates for #{@site.name}")
   end
 end
